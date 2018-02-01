0ff473243412067edd84535c74db453768463110
Artemii <pingvixD@yandex.ru>
2018-02-01 16:09:15 +0300

    100% parsing ratious

--- modules/clbookmaker.py
+++ modules/clbookmaker.py
@@ -1,13 +1,39 @@
+class Token:
+    def __init__(self, index, token):
+        self.index = index
+        self.token = token
+        self.ratio = []
+
+    def add_ratio(self, ratio):
+        self.ratio.extend(ratio)
+
+    def print_token(self):
+        print("Index:", self.index)
+        print(self.ratio)
+
 class Bookmaker:
-    def __init__(self, name_bookmaker, ratio_1, ratio_2):
+    def __init__(self, id_bookmaker, status):
         self.id_bookmaker = id_bookmaker
-        self.ratio_1 = ratio_1
-        self.ratio_2 = ratio_2
+        self.status = status
+        self.ratious = []
+
+    def add_tokens(self, array):
+        for key in array:
+            index = array.index(key)
+            self.ratious.append(Token(index, key))
+
+    def add_ratio(self, token, ratio):
+        for key in self.ratious:
+            if key.token == token:
+                key.add_ratio(ratio)
+
+    def get_id(self):
+        return self.id_bookmaker
 
     def print_bookmaker(self):
-        print('Bookmaker: ', self.name_bookmaker)
-        print('1: ', self.ratio_1)
-        print('2: ', self.ratio_2)
+        print('Bookmaker: ', self.id_bookmaker)
+        for key in self.ratious:
+            key.print_token()
 
     def check_ratio_errors(self):
         if self.ratio_1 == 'Higher' and self.ratio_2 == 'Higher':
@@ -16,15 +42,4 @@ class Bookmaker:
             return True
 
     def return_ratio(self, result):
-        if result == '1':
-            if self.check_ratio_errors() and (self.ratio_1 == 'Higher' or self.ratio_1 == 'Lower'):
-                return self.ratio_1
-            else:
-                return
-        elif result == '2':
-            if self.check_ratio_errors() and (self.ratio_2 == 'Higher' or self.ratio_2 == 'Lower'):
-                return self.ratio_2
-            else:
-                return
-        else:
-            print('Error with result!')
+        pass
--- modules/clratio.py
+++ modules/clratio.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 
+import json
+import re
 from modules.clbookmaker import Bookmaker
 
 class MatchRatio:
@@ -9,14 +11,59 @@ class MatchRatio:
         self.portal_request = portal_request
         self.bookmakers = []
 
+    def dict_to_list(self, elem):
+        if type(elem).__name__ == "dict":
+            return list(elem.values())
+        else:
+            return elem
+
     def run(self): #Нахождение букмекерский контор и их коэффициентов
-        json = self.portal_request.ratio_request()
-        print(json)
+        json_callback = self.portal_request.ratio_request()
+        json_string = re.search('[{](.+)[}]', json_callback).group(0)
+        data = json.loads(json_string)
+        oddsdata = data.get('d').get('oddsdata')
+        back = oddsdata.get('back')
+        array = list(back.values())
+        outcomeid = self.dict_to_list(array[0].get('OutcomeID'))
+        odds = array[0].get('odds')
+        change_time = array[0].get('change_time')
+        history_ratious_2 = []
+        for key in odds:
+            odd = self.dict_to_list(odds.get(key))
+            time = self.dict_to_list(change_time.get(key))
+            for i in range(0, len(odd)):
+                history_ratious_2.append({"bookmaker": key,
+                                          "token": outcomeid[i],
+                                          "ratio": [[str(odd[i]), 0, time[i]]]})
+        act = array[0].get('act')
+        bookmakers_ids = act.keys()
+        for key in bookmakers_ids:
+            status = act.get(key)
+            self.bookmakers.append(Bookmaker(key, status))
+        history = data.get('d').get('history')
+        history_back = history.get("back")
+        history_ratious = []
+        array_tokens = []
+        for key in history_back:
+            array_tokens.append(key)
+            token = history_back.get(key)
+            for bkey in token:
+                ratio = token.get(bkey)
+                history_ratious.append({"bookmaker" : bkey,
+                                        "token" : key,
+                                        "ratio" : ratio})
+        history_ratious.extend(history_ratious_2)
+        self.ratious(history_ratious, array_tokens)
 
-    def ratious(self): #Обработка разницы начального и конечного коэффициента
-        pass
+    def ratious(self, history_ratious, array_tokens): #Добавление в класс значений
+        for key in self.bookmakers:
+            key.add_tokens(array_tokens)
+            id_b = key.get_id()
+            for x in history_ratious:
+                if x["bookmaker"] == id_b:
+                    key.add_ratio(x["token"], x["ratio"])
 
-    def count_ratio(self, result):
+    def count_ratio(self, result): #Подсчет значений
         procents = 70
         lower, higher, error = [], [], []
         for key in self.bookmakers:
